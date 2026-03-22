9b0d2951eff7537c4312253d7d56c1a6b457bb7a
Ewout Mullink <ewoutmullink01@gmail.com>
2026-03-22 16:43:41 +0100

    logo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { StaticPageComponent } from './features/static-page/static-page.component';
+import { Home } from './features/home/home';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
-  { path: 'home', component: StaticPageComponent, data: { title: 'Home' } },
+  { path: 'home', component: Home, data: { title: 'Home' } },
   { path: 'agenda', component: StaticPageComponent, data: { title: 'Agenda' } },
   { path: 'kaartverkoop', component: StaticPageComponent, data: { title: 'Kaartverkoop' } },
   { path: 'over-ons', component: StaticPageComponent, data: { title: 'Over Ons' } },
